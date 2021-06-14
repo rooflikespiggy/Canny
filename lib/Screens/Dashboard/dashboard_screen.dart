5952c3bbe954cd7f05bba12a3847d6be5b24d5bd
ruthkangyr <ruthkangyr@gmail.com>
2021-06-14 16:43:59 +0800

    need edit dashboard

--- lib/Screens/Dashboard/dashboard_screen.dart
+++ lib/Screens/Dashboard/dashboard_screen.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:Canny/Shared/colors.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:Canny/Screens/Dashboard/expense_breakdown.dart';
+import 'package:flutter_rounded_progress_bar/flutter_rounded_progress_bar.dart';
+import 'package:flutter_rounded_progress_bar/rounded_progress_bar_style.dart';
 import 'package:intl/intl.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -29,6 +31,9 @@ class _DashboardScreenState extends State<DashboardScreen> {
   double teAmount = 0;
   int donutTouchedIndex;
   double totalExpensesAmount = 0;
+  double totalIncome = 0;
+  double balance = 0;
+  double percent = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -67,6 +72,12 @@ class _DashboardScreenState extends State<DashboardScreen> {
                           .where((category) => !category.isIncome)
                           .map((category) => category.categoryAmount[monthYear])
                           .reduce((value, element) => value + element);
+                      totalIncome = allCategories
+                          .where((category) => category.isIncome)
+                          .map((category) => category.categoryAmount[monthYear])
+                          .reduce((value, element) => value + element);
+                      balance = totalIncome - totalExpensesAmount;
+                      percent = ((totalExpensesAmount / totalIncome) * 100);
                       //print(totalCategoryAmount);
                       //print(teAmount);
                       //teSet = false;
@@ -116,6 +127,79 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                     ),
                                   ),
                                   SizedBox(height: 12),
+                                  /// Balance card
+                                  Visibility(
+                                    visible: totalIncome > 0,
+                                    child: Container(
+                                      width: 370,
+                                      child: Card(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                                        color: Colors.white.withOpacity(0.9),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                          children: <Widget> [
+                                            SizedBox(height: 15.0),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: <Widget> [
+                                                SizedBox(width: 15.0),
+                                                Text('Balance',
+                                                  style: TextStyle(
+                                                      fontSize: 16,
+                                                      fontFamily: "Lato",
+                                                      color: kDarkBlue
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                                              child: Divider(thickness: 2.0),
+                                            ),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: <Widget> [
+                                                SizedBox(width: 15.0),
+                                                RichText(
+                                                  text: TextSpan(
+                                                    text: 'Balance: ',
+                                                    style: TextStyle(
+                                                      fontFamily: "Lato",
+                                                      color: Colors.black54,
+                                                    ),
+                                                    children: <TextSpan>[
+                                                      TextSpan(
+                                                        text: '\$' + balance.toStringAsFixed(2),
+                                                        style: TextStyle(
+                                                          fontFamily: "Lato",
+                                                          color: Colors.black,
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              // TODO: find a nice way to display expense vs income
+                                              child: RoundedProgressBar(
+                                                percent: percent,
+                                                theme: RoundedProgressBarTheme.green,
+                                                childLeft: Text(totalExpensesAmount.toStringAsFixed(2),
+                                                    style: TextStyle(color: Colors.white)),
+                                                childRight: Text(balance.toStringAsFixed(2),
+                                                    style: TextStyle(color: Colors.white)),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  /// Expenses Breakdown card
                                   Container(
                                     width: 370,
                                     child: Card(
@@ -143,7 +227,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                             padding: const EdgeInsets.only(left: 12.0, right: 12.0),
                                             child: Divider(thickness: 2.0),
                                           ),
-                                          // TODO: decide where to put this total expenses
                                           Row(
                                             mainAxisAlignment: MainAxisAlignment.start,
                                             children: <Widget> [
@@ -157,7 +240,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                                   ),
                                                   children: <TextSpan>[
                                                     TextSpan(
-                                                      text: totalExpensesAmount.toStringAsFixed(2),
+                                                      text: '\$' + totalExpensesAmount.toStringAsFixed(2),
                                                       style: TextStyle(
                                                         fontFamily: "Lato",
                                                         color: totalExpensesAmount <= teAmount ? Colors.green : Colors.red,
@@ -227,44 +310,79 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                     ),
                                   ),
                                   SizedBox(height: 10),
-                                  // TODO: maybe put this into a card, refer to snapsheet statistics.dart
-                                  StreamBuilder(
-                                      stream: categoryCollection
-                                          .where('isIncome', isEqualTo: false)
-                                          .where('categoryAmount.$monthYear', isGreaterThan: 0)
-                                          .orderBy("categoryAmount.$monthYear", descending: true)
-                                          .snapshots(),
-                                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                                        if (snapshot.hasData) {
-                                          return Align(
-                                              alignment: Alignment.topCenter,
-                                              child: ListView.builder(
-                                                padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-                                                shrinkWrap: true,
-                                                physics: const NeverScrollableScrollPhysics(),
-                                                itemCount: snapshot.data.docs.length,
-                                                itemBuilder: (BuildContext context, int index) {
-                                                  final snapshotData = snapshot.data.docs[index];
-                                                  return ExpenseBreakdown(
-                                                    categoryName: snapshotData['categoryName'],
-                                                    categoryColorValue: snapshotData['categoryColorValue'],
-                                                    categoryIconCodePoint: snapshotData['categoryIconCodePoint'],
-                                                    categoryFontFamily: snapshotData['categoryFontFamily'],
-                                                    categoryFontPackage: snapshotData['categoryFontPackage'],
-                                                    categoryId: snapshotData.id,
-                                                    categoryAmount: snapshotData['categoryAmount'][monthYear],
-                                                    categoryPercentage: totalExpensesAmount <= teAmount
-                                                        ? ((snapshotData['categoryAmount'][monthYear] / teAmount) * 100)
-                                                        .toStringAsFixed(0)
-                                                        : ((snapshotData['categoryAmount'][monthYear] / totalExpensesAmount) * 100)
-                                                        .toStringAsFixed(0)
-                                                  );
-                                                },
-                                              )
-                                          );
+                                  /// Expenses Summary card
+                                  // TODO: find a nice way to display this card
+                                  Visibility(
+                                    visible: totalExpensesAmount > 0,
+                                    child: StreamBuilder(
+                                        stream: categoryCollection
+                                            .where('isIncome', isEqualTo: false)
+                                            .where('categoryAmount.$monthYear', isGreaterThan: 0)
+                                            .orderBy("categoryAmount.$monthYear", descending: true)
+                                            .snapshots(),
+                                        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                                          if (snapshot.hasData) {
+                                            return Container(
+                                              width: 370,
+                                              child: Card(
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                                                color: Colors.white.withOpacity(0.9),
+                                                child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                                  children: <Widget> [
+                                                    SizedBox(height: 15.0),
+                                                    Row(
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: <Widget> [
+                                                        SizedBox(width: 15.0),
+                                                        Text('Expenses Summary',
+                                                          style: TextStyle(
+                                                              fontSize: 16,
+                                                              fontFamily: "Lato",
+                                                              color: kDarkBlue
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    Padding(
+                                                      padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                                                      child: Divider(thickness: 2.0),
+                                                    ),
+                                                    Align(
+                                                        alignment: Alignment.topCenter,
+                                                        child: ListView.builder(
+                                                          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                                                          shrinkWrap: true,
+                                                          physics: const NeverScrollableScrollPhysics(),
+                                                          itemCount: snapshot.data.docs.length,
+                                                          itemBuilder: (BuildContext context, int index) {
+                                                            final snapshotData = snapshot.data.docs[index];
+                                                            return ExpenseBreakdown(
+                                                              categoryName: snapshotData['categoryName'],
+                                                              categoryColorValue: snapshotData['categoryColorValue'],
+                                                              categoryIconCodePoint: snapshotData['categoryIconCodePoint'],
+                                                              categoryFontFamily: snapshotData['categoryFontFamily'],
+                                                              categoryFontPackage: snapshotData['categoryFontPackage'],
+                                                              categoryId: snapshotData.id,
+                                                              categoryAmount: snapshotData['categoryAmount'][monthYear],
+                                                              categoryPercentage: totalExpensesAmount <= teAmount
+                                                                  ? ((snapshotData['categoryAmount'][monthYear] / teAmount) * 100)
+                                                                  .toStringAsFixed(0)
+                                                                  : ((snapshotData['categoryAmount'][monthYear] / totalExpensesAmount) * 100)
+                                                                  .toStringAsFixed(0)
+                                                            );
+                                                          },
+                                                        )
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            );
+                                          }
+                                          return CircularProgressIndicator();
                                         }
-                                        return CircularProgressIndicator();
-                                      }
+                                    ),
                                   ),
                                 ],
                               )
@@ -291,7 +409,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
       donutTouchedIndex = null;
     } else {
       donutTouchedIndex = i;
-      print(donutTouchedIndex);
     }
   }
 
